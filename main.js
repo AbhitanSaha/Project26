fce5c53f025bc562890f487f2573819c36f477c7
AbhitanSaha <72934030+AbhitanSaha@users.noreply.github.com>
2021-05-07 21:19:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+staus="";
 function preload() {
     
 }
 function setup() {
     canvas= createCanvas(450,350);
     canvas.position(50,100);
+    video= createCapture();
+    video.hide();
 }
 function draw() {
-    
+    image(video,0,0,450,350);   
+}
+function find() {
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+function modelLoaded() {
+    console.log("Model Loaded");
+    staus=true;
 }
